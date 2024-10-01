cebe1b3187434d5abf961fce4aaaabb87dafbd0b
awnzl <31739165+awnzl@users.noreply.github.com>
2024-10-01 10:05:12 -0400

    Finish build things

--- dockers/build.dockerfile
+++ dockers/build.dockerfile
@@ -1,6 +1,16 @@
 FROM golang:1.23.1-alpine AS builder
 WORKDIR /app_src
 
-RUN apk add --no-cache make
+RUN apk update && apk add --no-cache make protobuf-dev
+
+RUN go install google.golang.org/protobuf/cmd/protoc-gen-go@latest \
+    && go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+COPY ../cmd ./cmd
+COPY ../lib ./lib
+COPY ../go.mod ./go.mod
+COPY ../go.sum ./go.sum
+COPY ../Makefile ./Makefile
+
 RUN go mod download && go mod verify
 RUN make all
--- dockers/build.sh
+++ dockers/build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+BUILD_CONTEXT=$SCRIPT_DIR/..
+
 builder="currency_checker_builder"
-logfile="build.log"
+logfile="$SCRIPT_DIR/build.log"
+
+echo "start building" > $logfile 2>&1
 
 build_service() {
     local service_name=$1
     local dockerfile_path=$2
 
-    echo "Building $service_name..."
-    docker build --build-arg BUILDER=$builder -f $dockerfile_path -t "$service_name" >> $logfile 2>&1
+    build_cmd="docker build --build-arg BUILDER=$builder -f $dockerfile_path -t "$service_name" $BUILD_CONTEXT"
+    echo "Building $service_name..." >> $logfile 2>&1
+    echo "build command: '$build_cmd'" >> $logfile 2>&1
+    docker build --build-arg BUILDER=$builder -f $dockerfile_path -t "$service_name" $SCRIPT_DIR >> $logfile 2>&1
     if [ $? -ne 0 ]; then
         echo "Error: Failed to build $service_name. Check $logfile for details."
         exit 1
@@ -16,15 +23,17 @@ build_service() {
 }
 
 # builder
-docker build -f build.dockerfile -t $builder > $logfile 2>&1
+build_cmd="docker build -f $SCRIPT_DIR/build.dockerfile -t $builder $BUILD_CONTEXT"
+echo "build command: '$build_cmd'" >> $logfile 2>&1
+$build_cmd >> $logfile 2>&1
 if [ $? -ne 0 ]; then
     echo "Error: Failed to build builder image. Check $logfile for details."
     exit 1
 fi
 
 # services
-build_service "currency_checker" "currency_checker/dockerfile"
-build_service "price_collector" "price_collector/dockerfile"
-build_service "rank_collector" "rank_collector/dockerfile"
+build_service "currency_checker" $SCRIPT_DIR/"currency_checker/dockerfile"
+build_service "price_collector" $SCRIPT_DIR/"price_collector/dockerfile"
+build_service "rank_collector" $SCRIPT_DIR/"rank_collector/dockerfile"
 
 echo "All builds completed successfully. See $logfile for the full log."
